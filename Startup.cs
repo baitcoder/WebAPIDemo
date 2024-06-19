5f5122f2d491f8bc95bb73dd24748c64ff4083e6
baitcoder <103745511+baitcoder@users.noreply.github.com>
2024-06-19 13:31:53 +0530

    code uploaded

---
+++ Startup.cs
@@ -0,0 +1,72 @@
+using Hellang.Middleware.ProblemDetails;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.StaticFiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebAPIDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+      
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddControllers(options=>
+            {
+                options.ReturnHttpNotAcceptable = true; //to accepting other data format s
+            });
+        
+            services.AddProblemDetails(); // for handling customized response errors like to locate server errors
+            services.AddSingleton<FileExtensionContentTypeProvider>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIDemo", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIDemo v1"));
+            }
+
+            
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            logger.LogInformation("Application started");
+        }
+    }
+}
---
+++ StudentSubjectCollection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo
+{
+    public class StudentSubjectCollection
+    {
+
+        public static  StudentSubjectCollection Current = new StudentSubjectCollection();
+        public  List<Student> StudentsList;
+        public  List<Subject> SubjectsList;
+
+
+        public StudentSubjectCollection()
+        {
+            SubjectsList = new List<Subject>()
+            {
+                new Subject() { Sid = 1,SName = "Telugu"},
+                new Subject() { Sid = 2,SName = "Hindi"},
+                new Subject() { Sid = 3,SName = "English"},
+                new Subject() { Sid = 4,SName = "Maths"},
+                new Subject() { Sid = 5,SName = "Science"},
+                new Subject() { Sid = 6,SName = "Social"}
+            };
+
+            StudentsList = new List<Student>()
+            {
+                new Student(){Id=10,Name="A",Age=16,Subjects=new List<Subject>(){ new Subject() { Sid = 4,SName = "Maths"},
+                new Subject() { Sid = 5,SName = "Science"},
+                new Subject() { Sid = 6,SName = "Social"} } },
+               
+                new Student(){Id=20,Name="B",Age=13,Subjects=new List<Subject>(){ new Subject() { Sid = 4,SName = "Maths"},
+                new Subject() { Sid = 5,SName = "Science"},
+                new Subject() { Sid = 6,SName = "Social"}  } },
+                new Student(){Id=30,Name="C",Age=11,Subjects=new List<Subject>(){ new Subject() { Sid = 4,SName = "Maths"},
+                new Subject() { Sid = 5,SName = "Science"},
+                new Subject() { Sid = 6,SName = "Social"}  } }
+                
+
+            };
+
+           
+
+        }
+    }
+}
